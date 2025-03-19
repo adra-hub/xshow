cf2224a76b6cc0b829c6a86de2229bfdcf268720
adra-hub <90457545+adra-hub@users.noreply.github.com>
2025-03-19 10:57:31 +0000

    blank

--- config/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'installed_at' => '2024-11-23 20:27:42',
-  'users' => 
-  array (
-    'admin' => '$2y$10$kQ5GJRdguy49v8WmvJYco.JO0kjqncSf5pU8XN1aYjpImSdHh6kH2',
-    'user' => '$2y$10$y532uT9Xw3pptkW8uApl.u.V9rGPNt/KkYl5F5WGdrILt.v.woF26',
-  ),
-);
